7a0e62afedd555b46d6e2cf2b24d6b13d4952a21
unknown <kotbekareem74@gmail.com>
2024-03-03 23:25:33 +0200

    edits

--- resources/views/site/article.blade.php
+++ resources/views/site/article.blade.php
@@ -49,7 +49,7 @@
 <meta property="og:title" content="{{ $article->title }}">
 <meta property="og:description" content="{{ $article->content }}">
 <meta property="og:image" content="{{ "https://elgomhuriaeljadida.com" . $article->thumbnail_path }}">
-<meta property="og:url" content="https://elgomhuriaeljadida.com/article/{{$article->id}}">
+<meta property="og:url" content="https://elgomhuriaeljadida.com/article/{{$article->id}}?v=1">
 <meta property="og:type" content="article">
 <meta property="og:site_name" content="El gomhuria el Gdida">
 <meta property="og:locale" content="ar_Ar">
