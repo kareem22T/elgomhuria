5048dd53bdb6ca5a45850ca1162948b002d3a8d9
kareem22T <kotbekareem74@gmail.com>
2024-06-01 14:58:08 +0300

    edits

--- resources/views/site/home.blade.php
+++ resources/views/site/home.blade.php
@@ -17,11 +17,11 @@
 
             <div id="ticker-box" style="overflow: hidden; min-height: 40px;">
                 <ul style="padding: 0px; margin: 0px; position: relative; list-style-type: none;">
-                    
+
                     <li style="display: flex; justify-content: center; align-items: center; gap: 5px;    position: absolute; white-space: nowrap; right: -3543px; color: rgb(0, 0, 0);">
                         @foreach ($important_articles as $index => $important)
                           <a href="/article/{{$important->article->id}}" style="text-decoration: none; color:rgb(0, 0, 0); display: inline-flex;justify-content: center; align-items: center;gap: 12px;margin-right: 12px">
-                              {{$important->article->title}} 
+                              {{$important->article->title}}
                               @if ($index + 1 !== $important_articles->count())
                               <img src="{{ asset("/site/imgs/logo_t.png")}}" alt="" style="width: 20px">
                               @endif
@@ -37,7 +37,7 @@
                 else
                     startTicker('ticker-box', {speed: 1, delay:500});
             </script>
-    
+
         </div>
         </div>
 </div>
@@ -54,7 +54,7 @@ $more_visited = App\Models\Visit::with(['article' => function ($query) {
 <main class="home">
     <div class="container">
         <div class="col-1">
-            @if ($ads)                
+            @if ($ads)
                 <div class="ad_wrapper">
                     <section class="ad main">
                         @if ($ads->main_ad)
@@ -67,7 +67,7 @@ $more_visited = App\Models\Visit::with(['article' => function ($query) {
                 <section class="latest">
                     <div class="cat-head">
                         <div class="cat">
-                            الاحدث                    
+                            الاحدث
                         </div>
                         <span></span>
                     </div>
@@ -84,7 +84,7 @@ $more_visited = App\Models\Visit::with(['article' => function ($query) {
                     @if($more_visited && $more_visited->count() > 3)
                     <div class="cat-head" style="margin-top: 30px;">
                         <div class="cat">
-                            الاكثر قراءة                    
+                            الاكثر قراءة
                         </div>
                         <span></span>
                     </div>
@@ -112,10 +112,10 @@ $more_visited = App\Models\Visit::with(['article' => function ($query) {
             @if ($main_articles->count() > 0)
             <div class="swiper mainSwiper">
                 <div class="swiper-wrapper">
-                    @foreach ($main_articles as $item)   
+                    @foreach ($main_articles as $item)
                     @php
                         $main_article = App\Models\Article::find($item->article_id);
-                    @endphp                     
+                    @endphp
                     <a href="/article/{{$main_article->id}}" target="_blanck" class="swiper-slide">
                         <div class="thumbnail">
                             <img src="{{$main_article->thumbnail_path}}" alt="">
@@ -127,7 +127,7 @@ $more_visited = App\Models\Visit::with(['article' => function ($query) {
                                 <span>كتب: {{$main_article->author_name}}</span><br>
                                 {{Illuminate\Support\Str::limit($main_article->intro, 195)}}
                             </p>
-                            
+
                         </div>
                     </a>
                     @endforeach
@@ -137,7 +137,7 @@ $more_visited = App\Models\Visit::with(['article' => function ($query) {
                 <div class="mainSwiper-swiper-pagination"></div>
             </div>
             @endif
-            @if ($ads)                
+            @if ($ads)
                 <div class="ad_wrapper main_ad">
                     <section class="ad">
                         @if ($ads->main_ad)
@@ -150,7 +150,7 @@ $more_visited = App\Models\Visit::with(['article' => function ($query) {
 
             <section class="categories">
                 @if ($categories_per_home && count($categories_per_home )> 0)
-                    @foreach ($categories_per_home as $index => $category)                            
+                    @foreach ($categories_per_home as $index => $category)
                         <div class="swiper catSwiper">
                             <div class="swiper-wrapper">
                                 @foreach ($category->articles as $article)
@@ -162,7 +162,7 @@ $more_visited = App\Models\Visit::with(['article' => function ($query) {
                                             <div class="cat-head">
                                                 <span></span>
                                                 <a href="category/{{$category->id}}"  class="cat">
-                                                    {{$category->main_name}}                    
+                                                    {{$category->main_name}}
                                                 </a>
                                             </div>
                                             <a href="article/{{$article->id}}"  dir="rtl">
@@ -171,7 +171,7 @@ $more_visited = App\Models\Visit::with(['article' => function ($query) {
                                         </div>
                                     </div>
                                     @endforeach
-                                    <a href="/category/{{$category->id}}" class="swiper-slide" style="flex-direction: column;width: 513px; margin-right: 30px;display: flex;justify-content: center;align-items: center;font-size: 25px;font-weight: 700;color: var(--secondary-color);">
+                                    <a href="/category/{{$category->id}}" class="swiper-slide" style="flex-direction: column;width: 513px; margin-right: 30px;display: flex;justify-content: center;align-items: center;font-size: 25px;font-weight: 700;color: var(--secondary-color);  min-height: 300px;">
                                         {!! $category->icon !!}
                                         عرض كل مقالات قسم {{ $category->main_name}}
                                     </a>
@@ -182,7 +182,7 @@ $more_visited = App\Models\Visit::with(['article' => function ($query) {
                                 <div class="mainSwiper-swiper-button-next"><i class="fa-solid fa-angle-right"></i></div>
                             </div>
                         </div>
-                        @if ($ads && $index == 1)                
+                        @if ($ads && $index == 1)
                             <div class="ad_wrapper" style="margin-bottom: 1rem">
                                 <section class="ad">
                                     @if ($ads->mobile_ad_1)
@@ -190,11 +190,11 @@ $more_visited = App\Models\Visit::with(['article' => function ($query) {
                                     @endif
                                 </section>
                             </div>
-                        @endif        
+                        @endif
                     @endforeach
                 @endif
             </section>
-            @if ($ads)                
+            @if ($ads)
                 <div class="ad_wrapper">
                     <section class="ad">
                         @if ($ads->mobile_ad_2)
@@ -207,7 +207,7 @@ $more_visited = App\Models\Visit::with(['article' => function ($query) {
                 <section class="latest">
                     <div class="cat-head">
                         <div class="cat">
-                            الاحدث                    
+                            الاحدث
                         </div>
                         <span></span>
                     </div>
@@ -224,7 +224,7 @@ $more_visited = App\Models\Visit::with(['article' => function ($query) {
                     @if($more_visited && $more_visited->count() > 3)
                     <div class="cat-head" style="margin-top: 30px;">
                         <div class="cat">
-                            الاكثر قراءة                    
+                            الاكثر قراءة
                         </div>
                         <span></span>
                     </div>
@@ -245,7 +245,7 @@ $more_visited = App\Models\Visit::with(['article' => function ($query) {
                 </section>
                 @endif
 
-            @if ($ads)                
+            @if ($ads)
                 <div class="ad_wrapper">
                     <section class="ad">
                         @if ($ads->mobile_ad_3)
